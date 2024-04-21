d06a9c42cda80bf17701b5eef2f1008fb96d85a9
vivek <vivek.s.sheth@gmail.com>
2024-04-21 12:12:47 +1000

    added data clean code

--- starter/main.py
+++ starter/main.py
@@ -12,8 +12,24 @@ from typing import Union, Optional
 import pandas as pd
 import os
 import pickle
-from ml.data import process_data
+# from ml.data import process_data
 
 # path to saved artifacts
 savepath = './model'
 filename = ['trained_model.pkl', 'encoder.pkl', 'labelizer.pkl']
+
+# instantiate FastAPI app
+app = FastAPI(title="Inference API",
+              description="An API that takes a sample and runs an inference",
+              version="1.0.0")
+
+# load model artifacts on startup of the application to reduce latency
+
+
+@app.get("/")
+async def greetings():
+    return "Welcome to Fast Model API"
+
+
+if __name__ == '__main__':
+    pass
