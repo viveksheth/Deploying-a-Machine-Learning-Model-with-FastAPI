14db76152c2eebb09e8131f558d4d7416081cf66
vivek <vivek.s.sheth@gmail.com>
2024-04-21 11:51:36 +1000

    adding model code changes

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,19 @@
 # Put the code for your API here.
+
+"""
+Script for FastAPI instance and model interface 
+Author: Vivek Sheth
+Date: Apr 20, 2024
+
+"""
+
+from fastapi import FastAPI, HTTPException
+from typing import Union, Optional
+import pandas as pd
+import os
+import pickle
+from ml.data import process_data
+
+# path to saved artifacts
+savepath = './model'
+filename = ['trained_model.pkl', 'encoder.pkl', 'labelizer.pkl']
--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,7 +1,19 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+import logging
+import pandas as pd
+import multiprocessing
+from sklearn.metrics import fbeta_score, precision_score, recall_score, confusion_matrix
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
 
+logging.basicConfig(filename='journal.log',
+                    level=logging.INFO,
+                    filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 # Optional: implement hyperparameter tuning.
+
+
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
@@ -18,7 +30,28 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    parameters = {
+        'n_estimators': [10, 20, 30],
+        'max_depth': [5, 10],
+        'min_samples_split': [20, 50, 100],
+        'learning_rate': [1.0],  # 0.1,0.5,
+    }
+
+    njobs = multiprocessing.cpu_count() - 1
+    logging.info("Searching best hyperparameters on {} cores".format(njobs))
+
+    clf = GridSearchCV(GradientBoostingClassifier(random_state=0),
+                       param_grid=parameters,
+                       cv=3,
+                       n_jobs=njobs,
+                       verbose=2,
+                       )
+
+    clf.fit(X_train, y_train)
+    logging.info("********* Best parameters found ***********")
+    logging.info("BEST PARAMS: {}".format(clf.best_params_))
+
+    return clf
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +90,71 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    predictions = model.predict(X)
+
+    return predictions
+
+
+def compute_confusion_matrix(y, preds, labels=None):
+    """
+    Compute confusion matrix using the predictions and ground thruth
+    Inputs
+    ------
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    ------
+    confusionMatrix : confusion matrix for the provided prediction set
+    """
+    confusionMatrix = confusion_matrix(y, preds)
+    return confusionMatrix
+
+
+def compute_slices(df, feature, y, preds):
+    """
+    Compute the performance on slices for a given categorical feature
+    a slice corresponds to one value option of the categorical feature analyzed
+    ------
+    df: 
+        test dataframe pre-processed with features as column used for slices
+    feature:
+        feature on which to perform the slices
+    y : np.array
+        corresponding known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized
+
+    Returns
+    ------
+    Dataframe with
+        n_samples: integer - number of data samples in the slice
+        precision : float
+        recall : float
+        fbeta : float
+    row corresponding to each of the unique values taken by the feature (slice)
+    """
+    slice_options = df[feature].unique().tolist()
+    perf_df = pd.DataFrame(index=slice_options,
+                           columns=['feature', 'n_samples', 'precision', 'recall', 'fbeta'])
+    for option in slice_options:
+        slice_mask = df[feature] == option
+
+        slice_y = y[slice_mask]
+        slice_preds = preds[slice_mask]
+        precision, recall, fbeta = compute_model_metrics(slice_y, slice_preds)
+
+        perf_df.at[option, 'feature'] = feature
+        perf_df.at[option, 'n_samples'] = len(slice_y)
+        perf_df.at[option, 'precision'] = precision
+        perf_df.at[option, 'recall'] = recall
+        perf_df.at[option, 'fbeta'] = fbeta
+
+    # reorder columns in performance dataframe
+    perf_df.reset_index(names='feature value', inplace=True)
+    colList = list(perf_df.columns)
+    colList[0], colList[1] = colList[1], colList[0]
+    perf_df = perf_df[colList]
+
+    return perf_df
